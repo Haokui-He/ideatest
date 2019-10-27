7320cca49616d3bfd48ca6eb2e825886fc91ffb3
Haokui He <haokuihe@qq.com>
2019-10-27 20:48:33 +0800

    添加工程至git

---
+++ git_test/src/main/java/com/hhk/HelloTest.java
@@ -0,0 +1,13 @@
+package com.hhk;
+
+public class HelloTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
